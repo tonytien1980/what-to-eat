01aeda28cd055b975275a60ffd915e329896b762
TonyTien <oldtien_base@TonyTiendeMac-mini.local>
2026-04-05 12:50:20 +0800

    chore: remove legacy art asset module

--- src/assets/game-art.ts
+++
@@ -1,9 +0,0 @@
-import forestRuinsBackground from '../../images/fantasy-forest-ruins-background.png';
-import healingPotionIcon from '../../images/fantasy-healing-potion-icon.png';
-import mageKeyArt from '../../images/fantasy-mage-key-art.png';
-
-export const gameArt = {
-  forestRuinsBackground,
-  healingPotionIcon,
-  mageKeyArt,
-};
